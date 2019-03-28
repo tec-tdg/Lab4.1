bf1a08658383ef207562e09e8474d54ebfc403d5
Fera95 <ferarios95@gmail.com>
2019-03-27 22:02:44 -0600

    Se cambia el debounce
    Se pasa el debounce a ms

--- clock_div.sv
+++ clock_div.sv
@@ -5,7 +5,7 @@ module clock_div(input Clk_100M, output logic slow_clk
     reg [26:0]counter=0;
     always @(posedge Clk_100M)
     begin
-        counter <= (counter>=249999)?0:counter+1;
+        counter <= (counter>=299999)?0:counter+1;
         slow_clk <= (counter < 125000)?1'b0:1'b1;
     end
 endmodule
